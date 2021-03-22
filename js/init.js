dd99d01a04a95b2882d4534db98f5240d759f22b
Autentifica43UGR <81185589+Autentifica43UGR@users.noreply.github.com>
2021-03-22 15:50:41 -0600

    Create init.js

---
+++ js/init.js
@@ -0,0 +1,13 @@
+  
+/*  Initializa Firebase con la configuración del proyecto. Revisa la
+ * configuración en tu servidor de Firebase.  */
+// @ts-ignore
+firebase.initializeApp({
+    apiKey: "AIzaSyCgRott_dhN1pBF5yh3NV6ncp7NehH9kho",
+    authDomain: "autentificar43ugr.firebaseapp.com",
+    projectId: "autentificar43ugr",
+    storageBucket: "autentificar43ugr.appspot.com",
+    messagingSenderId: "1664571172",
+    appId: "1:1664571172:web:854ee8ca2f2b46694205b1",
+    measurementId: "G-T6LG5D82LJ"
+    });
